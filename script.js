ef6bc0b573908daf0886f1163352a7d6e4421809
VitaHub <hubenko_vitalii@mail.ua>
2016-04-01 15:17:52 +0300

    POST try 1

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-var app = angular.module("CustomerApp", ["ui.bootstrap"]);
+var app = angular.module("CustomerApp", ["ngResource","ui.bootstrap"]);
 
 app.controller('MainController', ['$scope','$http', 'authentication', function($scope, $http, authentication) {
 
 	authentication.then(function(data) {
-		$scope.token = data.data.token;
+		$scope.token = data;
 	});
+/*
+	authentication.serviceObject("java.script@test.com","strong_password").then
+		function(loginResult) {
+			$scope.token = loginResult;
+		},function(err) {
+			$scope.token = err;
+		})*/
+
 
 	$scope.searchType = "ID";
 	
@@ -34,7 +42,7 @@ app.controller('MainController', ['$scope','$http', 'authentication', function($
   	
 }]);
 
-app.factory('authentication', ['$http', function($http) {
+/*app.factory('authentication', ['$http', function($http) {
 	return $http.get('JSON/auth.json')
 		.then(function(data) {
 			return data;
@@ -42,14 +50,17 @@ app.factory('authentication', ['$http', function($http) {
 		.catch(function(err) {
 			return err;
 		});
-}]);
+}]);*/
 
-/*
-app.factory('authentication', ['$http', function($http) {
 
-	return $http.post('https://wc22.axonsoftware.biz/rest/auth/login', {
-        "email":"test@test.com",
- 		"password":"strong_password"
+/*app.factory('authentication', ['$http', function($http) {
+
+	return $http({
+		method: 'post',
+		url: 'https://wc22.axonsoftware.biz/rest/auth/login', 
+		data: { 
+        email:"java.script@test.com",
+ 		password:"strong_password" }
       })
 		.then(function(data) {
 			return data;
@@ -57,5 +68,25 @@ app.factory('authentication', ['$http', function($http) {
 		.catch(function(err) {
 			return err;
 		});
-}]);
-*/
+}]);*/
+
+app.factory('authentication', function($http) {
+
+	/*var LoginResource = $http.post("https://wc22.axonsoftware.biz/rest/auth/login");
+    var serviceObject = function (email, password) {
+        return LoginResource.save({}, {email: email, password: password}).$promise; //this promise will be fulfilled when the response is retrieved for this call
+    };
+    return serviceObject;*/
+
+
+	return $http(
+		method: 'POST',
+		url: 'https://wc22.axonsoftware.biz/rest/auth/login',
+		body: '{\r\n "email":"java.script@test.com",\r\n "password":"strong_password"\r\n}')
+		.then(function(data) {
+			return data;
+		})
+		.catch(function(err) {
+			return err;
+		});
+});
